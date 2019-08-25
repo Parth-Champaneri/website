0a70e1fc22f0b051b4d86d7a3412649fed84162d
Parth-Champaneri <parth.champaneri@hotmail.com>
2019-08-25 03:51:38 -0400

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontpageComponent
   ],
   imports: [
     BrowserModule,
